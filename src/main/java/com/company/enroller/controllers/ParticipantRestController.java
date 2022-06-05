18dc90d8721f304c21f4a768077dddd6bc20af7d
CaspaaCaspaa <kasper.kaczmarek@gmail.com>
2022-06-05 21:16:26 +0200

    first step done

---
+++ src/main/java/com/company/enroller/controllers/ParticipantRestController.java
@@ -0,0 +1,67 @@
+package com.company.enroller.controllers;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.company.enroller.model.Participant;
+import com.company.enroller.persistence.ParticipantService;
+
+@RestController
+@RequestMapping("/participants")
+public class ParticipantRestController {
+
+	@Autowired
+	ParticipantService participantService;
+
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public ResponseEntity<?> getParticipants() {
+		Collection<Participant> participants = participantService.getAll();
+		return new ResponseEntity<Collection<Participant>>(participants, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> getParticipant(@PathVariable("id") String login) {
+		Participant participant = participantService.findByLogin(login);
+		if (participant == null) {
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Participant>(participant, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public ResponseEntity<?> registerParticipant(@RequestBody Participant participant) {
+		String newLogin = participant.getLogin();
+		if (participantService.findByLogin(newLogin) == null) {
+			participantService.register(participant);
+			return new ResponseEntity<Participant>(participant, HttpStatus.CREATED);
+		}
+		return new ResponseEntity("Unable to create. A participant with login " + participant.getLogin() + " already exist.", HttpStatus.CONFLICT);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<?> removeParticipant(@PathVariable("id") String login) {
+		Participant participant = participantService.findByLogin(login);
+		if (participant == null) {
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
+		}
+		participantService.remove(participant);
+		return new ResponseEntity<Participant>(participant, HttpStatus.NO_CONTENT);
+	}
+
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<?> updateParticipant(@PathVariable("id") String login, String newPassword) {
+		Participant participant = participantService.findByLogin(login);
+		if (participant == null) {
+			return new ResponseEntity<String>("Unable to update. User " + participant.getLogin() + " not found.",
+					HttpStatus.NOT_FOUND);
+		}
+		participantService.update(participant, newPassword);
+		return new ResponseEntity<Participant>(participant, HttpStatus.OK);
+	}
+
+}
---
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -0,0 +1,53 @@
+package com.company.enroller.persistence;
+
+import java.util.Collection;
+
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Component;
+
+import com.company.enroller.model.Participant;
+
+@Component("participantService")
+public class ParticipantService {
+
+	DatabaseConnector connector;
+
+	public ParticipantService() {
+		connector = DatabaseConnector.getInstance();
+	}
+
+	public Collection<Participant> getAll() {
+		String hql = "FROM Participant";
+		Query query = connector.getSession().createQuery(hql);
+		return query.list();
+	}
+
+	public Participant findByLogin(String login) {
+		Participant searchedParticipant = (Participant) connector.getSession().get(Participant.class, login);
+		return searchedParticipant;
+	}
+
+	public Participant register(Participant participant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().save(participant);
+		transaction.commit();
+	return participant;
+	}
+
+	public void remove(Participant participant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().delete(participant);
+		transaction.commit();
+	}
+
+	public void update(Participant participant, String newPassword) {
+		participant.setPassword(newPassword);
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().update(participant);
+		transaction.commit();
+	}
+
+
+}
+
